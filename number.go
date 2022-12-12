940b31c9d796877533c345252616d5f10dfff345
aliifam <aliif.arief.m@gmail.com>
2022-12-12 22:46:15 +0700

    init & semangat

---
+++ number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("ini angka satu = ", 1)
+	fmt.Println("ini angka dua = ", 2)
+	fmt.Println("ini angka tiga setengah = ", 3.5)
+}
---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Aliif"))
+	fmt.Println("Aliif Arief")
+	fmt.Println("Aliif Arief Maulana")
+	fmt.Println(len("Aliif Arief Maulana"))
+	fmt.Println("Aliif"[0])
+}
